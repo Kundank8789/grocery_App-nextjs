20b824f043ee81eb34f8676ca77814a6c0685fb9
kundan <kundan8789@gmail.com>
2025-12-14 23:26:30 +0530

    Creating My Orders Page for User

---
+++ src/app/api/user/my-orders/route.ts
@@ -0,0 +1,18 @@
+import { auth } from "@/auth";
+import connectdb from "@/lib/db";
+import Order from "@/models/order.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req:NextRequest){
+    try {
+        await connectdb();
+        const session = await auth( )
+        const orders=await Order.find({user:session?.user?.id}).populate("user")
+        if(!orders){
+            return NextResponse.json({message:"No orders found"},{status:404})
+        }
+        return NextResponse.json(orders,{status:200})
+    } catch (error) {
+        return NextResponse.json({message:`Internal Server Error:${error}`},{status:500})
+    }
+}
--- src/app/user/my-orders/page.tsx
+++ src/app/user/my-orders/page.tsx
@@ -1,8 +1,84 @@
-import React from 'react'
+'use client'
+
+import { IOrder } from '@/models/order.model'
+import axios from 'axios'
+import { ArrowLeft, PackageSearch } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { motion } from 'motion/react'
+import UserOrderCard from '@/components/UserOrderCard'
 
 function MyOrder() {
+  const router = useRouter()
+  const [orders, setOrders] = useState<IOrder[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const getMyOrders = async () => {
+      try {
+        const result = await axios.get('/api/user/my-orders')
+        setOrders(result.data)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    getMyOrders()
+  }, [])
+
+  if (loading) {
+    return (
+      <div className='min-h-screen flex items-center justify-center'>
+        Loading your orders...
+      </div>
+    )
+  }
+
   return (
-    <div>page</div>
+    <div className='bg-linear-to-b from-white to-gray-100 min-h-screen w-full'>
+      
+      {/* HEADER */}
+      <div className='fixed top-0 left-0 w-full backdrop-blur-lg bg-white/70 shadow-sm border-b z-50'>
+        <div className='max-w-3xl mx-auto flex items-center gap-4 px-4 py-3'>
+          <button
+            className='p-2 bg-gray-100 rounded-full hover:bg-gray-200 active:scale-95 transition'
+            onClick={() => router.push('/')}
+          >
+            <ArrowLeft size={24} className='text-green-700' />
+          </button>
+          <h1 className='text-xl font-bold text-gray-800'>My Orders</h1>
+        </div>
+      </div>
+
+      {/* CONTENT */}
+      <div className='pt-20 max-w-3xl mx-auto px-4'>
+        {orders.length === 0 ? (
+          <div className='flex flex-col items-center text-center mt-20'>
+            <PackageSearch size={70} className='text-green-600 mb-4' />
+            <h2 className='text-xl font-semibold text-gray-700'>
+              No orders found
+            </h2>
+            <p className='text-gray-500 text-sm mt-1'>
+              You have not made any orders yet
+            </p>
+          </div>
+        ):<div className='mt-4 space-y-6'>
+          {orders.map((order,index)=>(
+            <motion.div
+            key={index}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            >
+            <UserOrderCard order={order}/>
+            </motion.div>
+          ))}
+          </div>
+        }
+      </div>
+
+    </div>
   )
 }
 
