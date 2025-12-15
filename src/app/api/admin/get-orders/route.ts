bb18b1229b2e50ff8fbb0670c0ba3ff1cfb0f5d3
kundan <kundan8789@gmail.com>
2025-12-15 17:58:43 +0530

    Creating Manage Orders Page For Admin

---
+++ src/app/api/admin/get-orders/route.ts
@@ -0,0 +1,13 @@
+import connectdb from "@/lib/db";
+import Order from "@/models/order.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req:NextRequest){
+    try {
+        await connectdb()
+        const orders=await Order.find({}).populate("user")
+        return NextResponse.json(orders,{status:200})
+    } catch (error) {
+        return NextResponse.json({message:`Internal Server Error:${error}`},{status:500})
+    }
+}
