b15ef706d0afb486e2ec717aeb7444b33191fc09
kundan <kundan8789@gmail.com>
2025-12-26 22:09:08 +0530

    add emit handler

---
+++ src/lib/emitEventHandler.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import React from 'react'
+
+function emitEventHandler(socketId: string, event: string, data: any) {
+  try {
+    await  axios.post(`${process.env.NEXT_PUBLIC_SOCKET_SERVER}/notify`, { socketId, event, data })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default emitEventHandler
